9fce15f2be8b408f77742b3350c9fc7aaec830b4
Aaron Holmes <aaron@aaronholmes.net>
2014-07-13 17:49:19 -0700

    Add initial project. Writes responses to a file.

---
+++ QuestionAsker/Questions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionAsker
+{
+    public static class Question
+    {
+        private static Random Random = new Random();
+
+        private static string[] Questions = new string[]
+        {
+            "How are you feeling?",
+            "What are you up to?",
+            "Is anything agitating you right now?",
+            "What have you accomplished today?"
+        };
+
+        public static string Get()
+        {
+            return Questions[Random.Next(Questions.Length)];
+        }
+    }
+}
